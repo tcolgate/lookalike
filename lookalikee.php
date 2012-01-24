6ddf7ce2ad09025ea83a6e223afdbbb3507076ed
root <root@gbgh-svmon-01.sig.ads>
2012-01-24 15:52:16 +0000

    Add the tities, provide a link to view the graphs

--- lookalikee.php
+++ lookalikee.php
@@ -13,6 +13,19 @@ $graph_width = read_graph_config_option("default_width");
 $graph_height = read_graph_config_option("default_height");
 $cols = read_graph_config_option("num_columns");
 
+$title_font_size = 0;
+if ((read_config_option("rrdtool_version")) != "rrd-1.0.x") {
+	if (read_graph_config_option("title_font") == "") {
+		if (read_config_option("title_font") == "") {
+			$title_font_size = 10;
+		}else {
+			$title_font_size = read_config_option("title_size");
+		}
+	}else {
+		$title_font_size = read_graph_config_option("title_size");
+	}
+};
+
 /* required for zoom out function */
 if ($graph_start == $graph_end) {
 	$graph_start--;
@@ -28,6 +41,7 @@ exec($cmd, $output);
 
 $currcol = 1;
 print "<table>";
+$done = array( );
 foreach ($output as $line){
   preg_match("/Match:([^:]*):.*/",$line,$matches);
   if($matches){
@@ -39,6 +53,7 @@ foreach ($output as $line){
     $matchedgraphs = api_get_graphs_from_datasource($dsid);
 
     foreach(array_keys($matchedgraphs) as $matchedgraph){
+        if(isset($done[$matchedgraph])) { break ;};
 
 	$graph = db_fetch_row("select
 		graph_templates_graph.height,
@@ -46,23 +61,7 @@ foreach ($output as $line){
 		from graph_templates_graph
 		where graph_templates_graph.local_graph_id=" . $matchedgraph);
 
-#	$graph_height = $graph["height"];
-#	$graph_width = $graph["width"];
         $graph_title = get_graph_title($matchedgraph);
-
-	if ((read_config_option("rrdtool_version")) != "rrd-1.0.x") {
-		if (read_graph_config_option("title_font") == "") {
-			if (read_config_option("title_font") == "") {
-				$title_font_size = 10;
-			}else {
-				$title_font_size = read_config_option("title_size");
-			}
-		}else {
-			$title_font_size = read_graph_config_option("title_size");
-		}
-	}else {
-		$title_font_size = 0;
-	};
 
 	if($currcol == 0) {
           print "<tr>";
@@ -72,22 +71,31 @@ foreach ($output as $line){
 			<table width='1' cellpadding='0'>
 				<tr>
 					<td>
+                                                <a href='<?php print $config['url_path'] . "graph.php?action=view&local_graph_id=" . $matchedgraph ?>'>
 						<img src='<?php print htmlspecialchars($config['url_path'] . "graph_image.php?graph_nolegend=true&local_graph_id=" . $matchedgraph . "&graph_start=" . $graph_start . "&graph_end=" . $graph_end . "&graph_height=" . $graph_height . "&graph_width=" . $graph_width . "&title_font_size=" . $title_font_size);?>' border='0' alt='<?php print htmlspecialchars($graph_title);?>'>
+						</a>
+					</td>
+				</tr>
+				<tr>
+					<td>
+                                                <center><?php print $graph_title ?></center>
 					</td>
 				</tr>
 			</table>
 		</td>
-	<?php
-	if($currcol == $cols) {
-          print "</tr>";
-          $currcol = 1;
-        } else {
-          $currcol ++;
-        };
+      <?php
+      $done[$matchedgraph] = 1;
+      if($currcol == $cols) {
+        print "</tr>";
+        $currcol = 1;
+      } else {
+        $currcol ++;
+      };
     };
   }; 
 };
 print "</table>";
+var_dump($done);
 
 
 include_once($config["base_path"]."/include/bottom_footer.php");
