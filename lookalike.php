736856ae5e206a93788e3fcca7f6576ca770442c
Tristan Colgate-McFarlane <tcolgate@gmail.com>
2012-01-25 11:48:24 +0000

    Add some additional license info

--- lookalike.php
+++ lookalike.php
@@ -1,4 +1,23 @@
 <?php
+/*
+ ex: set tabstop=4 shiftwidth=4 autoindent:
+ +-------------------------------------------------------------------------+
+ | Copyright (C) 2011 - 2012 Tristan Colgate-McFarlane                     |
+ |                                                                         |
+ | This program is free software; you can redistribute it and/or           |
+ | modify it under the terms of the GNU General Public License             |
+ | as published by the Free Software Foundation; either version 2          |
+ | of the License, or (at your option) any later version.                  |
+ |                                                                         |
+ | This program is distributed in the hope that it will be useful,         |
+ | but WITHOUT ANY WARRANTY; without even the implied warranty of          |
+ | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the           |
+ | GNU General Public License for more details.                            |
+ +-------------------------------------------------------------------------+
+ | https://github.com/tcolgate/lookalike                                   |
+ +-------------------------------------------------------------------------+
+*/
+
 
 chdir('../../');
 include_once("./include/auth.php");
